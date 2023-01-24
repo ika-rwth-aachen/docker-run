4afe896d610951d9aa6fccad73c9fe9429b77df5
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2023-01-24 12:07:01 +0100

    add comments and small changes

--- generateDockerRunCommand.py
+++ generateDockerRunCommand.py
@@ -45,42 +45,52 @@ def runCommand(str, *args, **kwargs) -> str:
     return output.stdout.decode(), output.stderr.decode()
 
 def buildDockerRunCommand(args, unknown_args) -> str:
-    # build cmd
 
     OS = subprocess.run(['uname', '-s'], capture_output=True).stdout.decode()
     ARCH = subprocess.run(['uname', '-m'], capture_output=True).stdout.decode()
 
+    # check for running container -> docker exec
     runningContainers = runCommand('docker ps --format "{{.Names}}"')[0]
     runningContainers = runningContainers.split('\n')
     if args.name in runningContainers:
         print(f"Attatch to running container <{args.name}> ...")
         cmd = ' '.join(args.cmd) if args.cmd else 'bash'
         docker_command = ['docker', 'exec', '-it', args.name, cmd]
-                return docker_command
+        return docker_command
 
+    # init docker run command
+    print("Start new Container...")
     docker_command = ['docker', 'run']
-
+    
+    # check for gpu support -> default: use gpu
     if not args.no_gpu:
         if ARCH == "x86_64":
             docker_command += ['--gpus', 'all'] # "normal" Workstation
+            print("\t - with GPU-Support")
         elif ARCH == "aarch64":
             docker_command += ['--runtime', 'nvidia'] # Orin
+            print("\t - with GPU-Support")
         # else (e.g. Mac) -> no gpu
 
+    # default: run in interactive mode
     if not args.no_it:
+        print("\t - interactive")
         docker_command += ['-it']
 
+    # default: remove container after exiting
     if not args.no_rm:
         print("\t - remove after exiting")
         docker_command += ['--rm']
 
+    # default: run with gui forewarding
     if not args.no_x11:
+        print("\t - With GUI-Forewarding")
         XSOCK='/tmp/.X11-unix'  # to support X11 forwarding in isolated containers on local host (not thorugh SSH)
         XAUTH=tempfile.NamedTemporaryFile(prefix='.docker.xauth.', delete=False)
         xauth_output = subprocess.run('xauth nlist $DISPLAY', stdout=subprocess.PIPE, shell=True, env=os.environ)
         xauth_output = "ffff" + xauth_output.stdout.decode()[4:]
         subprocess.run(f'xauth -f {XAUTH.name} nmerge - 2>/dev/null', input=xauth_output.encode(), shell=True)
-        subprocess.run(['chmod', '777', XAUTH.name])
+        os.chmod(XAUTH.name, 0o777)
 
         DISPLAY=os.environ.get('DISPLAY')
         if not args.no_isolated:
@@ -93,11 +103,15 @@ def buildDockerRunCommand(args, unknown_args) -> str:
         docker_command += ['-e', f'XAUTHORITY={XAUTH.name}']
         docker_command += ['-v', f'{XAUTH.name}:{XAUTH.name}']
         docker_command += ['-v', f'{XSOCK}:{XSOCK}']
-    
+
+    # add --name flag to docker_command
     if args.name:
         docker_command += ['--name', args.name]
+
+    # add all unknown args (docker run args) to docker_command
     docker_command += unknown_args
 
+    # add image and cmd to docker_command
     if args.image:
         docker_command += [args.image]
     if args.cmd:
@@ -111,7 +125,7 @@ def buildDockerRunCommand(args, unknown_args) -> str:
 
 def main():
 
-    args, unknown_args = parseArguments()
+    args, unknown_args = parseArguments()    
     cmd = buildDockerRunCommand(args, unknown_args)
     print(' '.join(cmd), file=sys.stderr)
 
