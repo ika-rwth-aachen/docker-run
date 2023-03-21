50034d2b602537014ca86a460f9051dd1166d408
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2023-03-21 11:25:39 +0100

    only install if lines do not exist in bashrc

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DOCKER_RUN_DIR="$(realpath $(dirname ${0}))"
-echo "export PATH=\"${DOCKER_RUN_DIR}/bin:\$PATH\"" >> ~/.bashrc
-echo "source ${DOCKER_RUN_DIR}/bash-completion/docker-run" >> ~/.bashrc
+grep -qxF "export PATH=\"${DOCKER_RUN_DIR}/bin:\$PATH\"" ~/.bashrc || echo "export PATH=\"${DOCKER_RUN_DIR}/bin:\$PATH\"" >> ~/.bashrc
+grep -qxF "source ${DOCKER_RUN_DIR}/bash-completion/docker-run" ~/.bashrc || echo "source ${DOCKER_RUN_DIR}/bash-completion/docker-run" >> ~/.bashrc
 source ~/.bashrc
