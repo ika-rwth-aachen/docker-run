da5ea35df89195848112ce4749c35d430267334e
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2023-05-23 10:24:27 +0200

    add script to install bash completion

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DOCKER_RUN_DIR="$(realpath $(dirname ${BASH_SOURCE[0]}))"
-grep -qxF "export PATH=\"${DOCKER_RUN_DIR}/bin:\$PATH\"" ~/.bashrc || echo "export PATH=\"${DOCKER_RUN_DIR}/bin:\$PATH\"" >> ~/.bashrc
-grep -qxF "source ${DOCKER_RUN_DIR}/bash-completion/docker-run" ~/.bashrc || echo "source ${DOCKER_RUN_DIR}/bash-completion/docker-run" >> ~/.bashrc
-source ~/.bashrc
